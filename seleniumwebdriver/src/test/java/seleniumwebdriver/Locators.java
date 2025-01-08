fffbae9d0cb4b813e778338ca19ad1114f55bedc
karunakunde <karunaautomationtester@gmail.com>
2025-01-08 14:09:31 +0530

    first commit

--- seleniumwebdriver/src/test/java/seleniumwebdriver/Locators.java
+++ seleniumwebdriver/src/test/java/seleniumwebdriver/Locators.java
@@ -1,11 +1,14 @@
 package seleniumwebdriver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Locators {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//dfgdfgfcvbvrejhjh
-		
+
+		WebDriver webDriver = new ChromeDriver();		
 	}
 
 }
